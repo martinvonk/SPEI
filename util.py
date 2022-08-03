92678320d2e149e1e69e57c8be1d86de1f0e6242
Martin Vonk <vonk.mart@gmail.com>
2022-08-03 22:33:25 +0200

    codacy

--- util.py
+++ util.py
@@ -1,9 +1,12 @@
 from pandas import Series, DataFrame
 
+
 def check_series(series):
 
     if not isinstance(series, Series):
         if isinstance(series, DataFrame):
-            raise TypeError(f'Please convert pandas DataFrame to a Series using .squeeze()')
+            raise TypeError(
+                f'Please convert pandas DataFrame to a Series using .squeeze()')
         else:
-            raise TypeError(f'Please provide a Pandas Series instead of {type(series)}')
+            raise TypeError(
+                f'Please provide a Pandas Series instead of {type(series)}')
