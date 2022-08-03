634a774772eff93337fcdd0a851c6b3cbfa4e6cd
Martin Vonk <vonk.mart@gmail.com>
2022-08-03 22:04:21 +0200

    Add option to plot the (cumulative) distribution for each month

--- plot.py
+++ plot.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 from numpy import meshgrid, linspace
+from calendar import month_name
 
-def plot_si(si, figsize=(8,4)):
+
+def si(si, figsize=(8, 4)):
 
     _, ax = plt.subplots(figsize=figsize)
 
@@ -15,11 +17,37 @@ def plot_si(si, figsize=(8,4)):
     nodroughts = si.to_numpy(copy=True)
     nodroughts[nodroughts < 0] = 0
 
-
     x, y = meshgrid(si.index, linspace(nmin, nmax, 100))
-    ax.contourf(x, y, y, cmap=plt.cm.seismic_r, levels=linspace(nmin, nmax, 100))
+    ax.contourf(x, y, y, cmap=plt.cm.seismic_r,
+                levels=linspace(nmin, nmax, 100))
     ax.fill_between(x=si.index, y1=droughts, y2=nmin, color='w')
     ax.fill_between(x=si.index, y1=nodroughts, y2=nmax, color='w')
     ax.set_ylim(nmin, nmax)
 
     return ax
+
+
+def dist(series, dist, cumulative=False, cmap='Set3', figsize=(8, 10), legend=True):
+
+    _, axs = plt.subplots(4, 3, figsize=figsize, sharey=True, sharex=True)
+    ax = axs.ravel()
+    cm = plt.get_cmap(cmap)
+
+    for i, month in enumerate(range(1, 13)):
+        data = series[series.index.month == month].sort_values()
+        *pars, loc, scale = dist.fit(data)
+        cdf = dist.cdf(data, pars, loc=loc, scale=scale)
+        ax[i].hist(data, color=cm(i), alpha=0.2, density=True,
+                   cumulative=cumulative, label=f'Density')
+        if cumulative:
+            ax[i].plot(data, cdf, color=cm(
+                i), label=f'{loc=:0.2f}\n{scale=:0.2f}')
+        else:
+            x = linspace(min(data), max(data))
+            ax[i].plot(x, dist.pdf(x, pars, loc=loc, scale=scale), color=cm(
+                i), label=f'{dist.name.capitalize()} fit:\n{loc=:0.2f}\n{scale=:0.2f}')
+        ax[i].set_title(month_name[month])
+        if legend:
+            ax[i].legend()
+
+    return axs
