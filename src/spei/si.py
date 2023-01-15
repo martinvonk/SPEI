d8297ebb0025eb9cfc04ff19dc95627c1f6dc491
Martin Vonk <vonk.mart@gmail.com>
2023-01-15 10:08:29 +0100

    add type hinting

--- src/spei/si.py
+++ src/spei/si.py
@@ -1,16 +1,39 @@
-from typing import Any
 from pandas import Series
 from numpy import linspace
 from scipy.stats import norm, gamma, fisk, genextreme
 from .utils import check_series
 
+# Type Hinting
+from typing import Optional
+from .typing import ContinuousDist
+
 
 def get_si_ppf(
     series: Series,
-    dist: Any,
+    dist: ContinuousDist,
     sgi: bool = False,
     prob_zero: bool = False,
 ) -> Series:
+    """Internal helper function to calculate drought index
+
+    Parameters
+    ----------
+    series : Series
+        Series with observations
+    dist : ContinuousDist
+        Continuous distribution from the SciPy library
+    sgi : bool, optional
+        Whether to caclulate the standardized groundwater index or not, by
+        default False
+    prob_zero : bool, optional
+        Apply logic to observations of zero and calculate the probability
+        seperately, by default False
+
+    Returns
+    -------
+    Series
+        Series with probability point function ppf
+    """
 
     check_series(series)
 
@@ -61,7 +84,9 @@ def sgi(series: Series) -> Series:
     return get_si_ppf(series, None, sgi=True)
 
 
-def spi(series: Series, dist: Any = None, prob_zero: bool = False) -> Series:
+def spi(
+    series: Series, dist: Optional[ContinuousDist] = None, prob_zero: bool = False
+) -> Series:
     """Method to compute the Standardized Precipitation Index [spi_2002]_.
 
     Parameters
@@ -69,7 +94,7 @@ def spi(series: Series, dist: Any = None, prob_zero: bool = False) -> Series:
     series: pandas.Series
         Pandas time series of the precipitation. Time series index
         should be a pandas DatetimeIndex.
-    dist: scipy.stats._continuous_distns
+    dist: scipy.stats.rv_continuous
         Can be any continuous distribution from the scipy.stats library.
         However, for the SPI generally the Gamma probability density
         function is recommended. Other appropriate choices could be the
@@ -89,21 +114,23 @@ def spi(series: Series, dist: Any = None, prob_zero: bool = False) -> Series:
        22, 1571-1592, 2002.
     """
 
-    if dist == None:
+    if dist is None:
         dist = gamma
 
     return get_si_ppf(series, dist, prob_zero)
 
 
-def spei(series: Series, dist: Any = None) -> Series:
-    """Method to compute the Standardized Precipitation Evaporation Index [spei_2010]_.
+def spei(series: Series, dist: Optional[ContinuousDist] = None) -> Series:
+
+    """Method to compute the Standardized Precipitation Evaporation Index
+    [spei_2010]_.
 
     Parameters
     ----------
     series: pandas.Series
         Pandas time series of the precipitation. Time series index
         should be a pandas DatetimeIndex.
-    dist: scipy.stats._continuous_distns
+    dist: scipy.stats.rv_continuous
         Can be any continuous distribution from the scipy.stats library.
         However, for the SPEI generally the log-logistic (fisk) probability
         density function is recommended. Other appropriate choices could be
@@ -121,13 +148,13 @@ def spei(series: Series, dist: Any = None) -> Series:
        Journal of Climate, 23, 1696-1718, 2010.
     """
 
-    if dist == None:
+    if dist is None:
         dist = fisk  # log-logistic
 
     return get_si_ppf(series, dist)
 
 
-def ssfi(series: Series, dist: Any = None) -> Series:
+def ssfi(series: Series, dist: Optional[ContinuousDist] = None) -> Series:
     """Method to compute the Standardized StreamFlow Index [ssfi_2020]_.
 
     Parameters
@@ -135,7 +162,7 @@ def ssfi(series: Series, dist: Any = None) -> Series:
     series: pandas.Series
         Pandas time series of the precipitation. Time series index
         should be a pandas DatetimeIndex.
-    dist: scipy.stats._continuous_distns
+    dist: scipy.stats.rv_continuous
         Can be any continuous distribution from the scipy.stats library.
         However, for the SSFI generally the gamma probability density
         function is recommended. Other appropriate choices could be the
@@ -153,7 +180,7 @@ def ssfi(series: Series, dist: Any = None) -> Series:
        and nonparametric methods. Water Resources Research, 56, 2020.
     """
 
-    if dist == None:
+    if dist is None:
         dist = genextreme
 
     return get_si_ppf(series, dist)
