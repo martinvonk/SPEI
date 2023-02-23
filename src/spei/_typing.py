25eac45d4aa9e83a8cf5a34e9dae4e10ee2e9a4e
Martin Vonk <vonk.mart@gmail.com>
2023-02-23 21:53:31 +0100

    Update _typing.py
    fix tests

--- src/spei/_typing.py
+++ src/spei/_typing.py
@@ -3,10 +3,10 @@ from typing import Any, Union
 
 from matplotlib.axes import Axes
 from numpy import float64 as npfloat64
-from numpy.typing import npNDArray
+from numpy.typing import NDArray as npNDArray
 from scipy.stats._continuous_distns import rv_continuous
 
 ContinuousDist = Union[Any, rv_continuous]
 Axes = Union[Any, Axes]
-NDarray = npNDArray
+NDArray = npNDArray
 float64 = npfloat64
