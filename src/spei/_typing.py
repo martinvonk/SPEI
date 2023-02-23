f2b6f2e02fb855b807523b77772eb427eee3fffc
Martin Vonk <vonk.mart@gmail.com>
2023-02-23 14:16:59 +0100

    Update _typing.py
    redefine numpy types

--- src/spei/_typing.py
+++ src/spei/_typing.py
@@ -2,9 +2,11 @@
 from typing import Any, Union
 
 from matplotlib.axes import Axes
-from numpy import float64
-from numpy.typing import NDArray
+from numpy import float64 as npfloat64
+from numpy.typing import npNDArray
 from scipy.stats._continuous_distns import rv_continuous
 
 ContinuousDist = Union[Any, rv_continuous]
 Axes = Union[Any, Axes]
+NDarray = npNDArray
+float64 = npfloat64
