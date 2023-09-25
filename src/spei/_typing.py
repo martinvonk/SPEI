a57bfa514e080433e95444416a8b09c9de0f45aa
Martin Vonk <vonk.mart@gmail.com>
2023-09-25 19:34:39 +0200

    update type hint for axes

--- src/spei/_typing.py
+++ src/spei/_typing.py
@@ -1,12 +1,11 @@
 # flake8: noqa
-from typing import Any, TypeVar, Union
+from typing import Any, Union
 
-from matplotlib.axes import Axes as mplAxes
 from numpy import float64
 from numpy.typing import NDArray
 from scipy.stats._continuous_distns import rv_continuous
 
 ContinuousDist = Union[Any, rv_continuous]
-Axes = TypeVar("Axes", bound=Union[mplAxes, Any])
-NDArrayAxes = NDArray[Any]
+Axes = Any
+NDArrayAxes = NDArray[Axes]
 NDArrayFloat = NDArray[float64]
