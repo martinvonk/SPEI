7cdadac20a1c18b9786cc3ea401901777a22d355
Martin Vonk <vonk.mart@gmail.com>
2023-09-25 19:09:40 +0200

    Update _typing.py
    Type argument "Union[Any, Axes]" of "NDArray" must be a subtype of "generic"

--- src/spei/_typing.py
+++ src/spei/_typing.py
@@ -1,12 +1,12 @@
 # flake8: noqa
-from typing import Any, Union
+from typing import Any, Union, TypeVar
 
-from matplotlib.axes import Axes as mplAxes
-from numpy import float64
+# from matplotlib.axes import Axes
+from numpy import float64, generic
 from numpy.typing import NDArray
 from scipy.stats._continuous_distns import rv_continuous
 
 ContinuousDist = Union[Any, rv_continuous]
-Axes = Union[Any, mplAxes]
+Axes = TypeVar("Axes", bound=generic, covariant=True)
 NDArrayAxes = NDArray[Axes]
 NDArrayFloat = NDArray[float64]
