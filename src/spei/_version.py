c08a2dfc48e141b0c00cf004fd112fe1ea23608f
Martin Vonk <vonk.mart@gmail.com>
2024-11-06 12:10:27 +0100

    update show versions

--- src/spei/_version.py
+++ src/spei/_version.py
@@ -5,12 +5,13 @@ __version__ = "0.5.2"
 
 
 def show_versions() -> str:
-    msg = f"Versions\npython: {python_version()}\nspei: {__version__}\n"
+    msg = f"python: {python_version()}\nspei: {__version__}\n"
 
     requirements = metadata.requires("spei")
     if requirements:
         deps = [x for x in requirements if "extra" not in x]
         for dep in deps:
-            msg += f"{dep}: {metadata.version(dep)}\n"
+            msg += f"{dep}: {metadata.version(dep)}"
+            msg += "\n" if deps.index(dep) < len(deps) - 1 else ""
 
     return msg
