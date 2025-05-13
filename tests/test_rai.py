aea3ff04e21260e0d46a8f7ca8e8d78b73ddd4dd
martinvonk <vonk.mart@gmail.com>
2025-05-13 15:51:36 +0200

    ruff

--- tests/test_rai.py
+++ tests/test_rai.py
@@ -7,7 +7,9 @@ def test_rai(prec: Series) -> None:
     precrs = prec.resample("MS").sum()
     rai_result = rai(precrs)
     assert isinstance(rai_result, Series), "RAI result is not a pandas Series"
-    assert len(rai_result) == len(precrs), "RAI result length does not match input length"
+    assert len(rai_result) == len(precrs), (
+        "RAI result length does not match input length"
+    )
 
 
 def test_mrai(prec: Series) -> None:
