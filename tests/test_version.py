09b7031ab7f67b51a618dd6d664135d379be8956
martinvonk <vonk.mart@gmail.com>
2026-01-04 18:57:17 +0100

    add test

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,6 +5,11 @@ def test_version() -> None:
     assert isinstance(si.__version__, str)
     assert si.__version__.count(".") == 2
 
+def test_get_versions():
+    versionsd = si._version.get_versions()
+    assert isinstance(versionsd, dict)
+    assert "python" in versionsd
+    assert "spei" in versionsd
 
 def test_show_versions():
     msg = si.show_versions()
