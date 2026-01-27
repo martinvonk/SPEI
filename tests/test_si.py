0f98c879d51e92bf2e8c51f93f8914418088764f
martinvonk <vonk.mart@gmail.com>
2026-01-27 22:59:53 +0100

    fix tests and add test for nst

--- tests/test_si.py
+++ tests/test_si.py
@@ -1,3 +1,4 @@
+import pytest
 from pandas import DataFrame, Series, Timestamp
 from scipy.stats import norm
 
@@ -139,10 +140,29 @@ def test_ppf_nsf(prec: Series) -> None:
 
 
 def test_si_predict(prec: Series) -> None:
-    si = SI(prec, dist=norm, timescale=30, fit_freq="MS")
+    timescale = 30
+    si = SI(prec, dist=norm, timescale=timescale, fit_freq="MS")
     si.fit_distribution()
     pred = si.predict(prec)
     assert isinstance(pred, Series), "Predict result should be a Pandas Series"
-    assert len(pred) == len(prec), (
+    assert len(pred) == (len(prec) - timescale - 1), (
         "Predict result length does not match input series length"
     )
+
+
+def test_si_predict_with_normal_scores_transform(prec: Series) -> None:
+    """Test that prediction with normal_scores_transform raises NotImplementedError."""
+    timescale = 30
+    si = SI(
+        prec,
+        dist=norm,
+        timescale=timescale,
+        fit_freq="MS",
+        normal_scores_transform=True,
+    )
+    with pytest.raises(NotImplementedError) as excinfo:
+        si.predict(prec)
+        assert (
+            str(excinfo.value)
+            == "Prediction not supported when using normal-scores-transform."
+        )
